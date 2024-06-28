6490c674a9b71711f170bf0f4e270fac4d9999a8
Clement880 <clementlethata@gmail.com>
2024-06-27 17:23:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * 
+ * main -Entry start point
+ *
+ * Return: Always (0) success
+ *
+ */
+int main(void)
+{
+	char b;
+
+	b = 'a''
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
