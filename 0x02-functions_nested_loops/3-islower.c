50db6650b980071fd28d11a2debdb4e997892ed6
Clement880 <clementlethata@gmail.com>
2024-06-29 08:52:04 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower _Checks for lowercase character
+ * @C: The character to be checked
+ * Return: 1 for lowercase character or 0for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c == 122)
+	{
+	
+	return (c);
+	}
+	return (0);
+}
