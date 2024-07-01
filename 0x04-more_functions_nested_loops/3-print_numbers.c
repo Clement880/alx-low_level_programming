d1af88dfb0a58098ab07d10603d2202c17f5d232
Clement880 <clementlethata@gmail.com>
2024-07-01 05:42:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers, from 0 to 9
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int t;
+
+	for (t = 48; t <= 57; t++)
+	{
+		_putchar(t);
+	}
+	_putchar('\n');
+}
