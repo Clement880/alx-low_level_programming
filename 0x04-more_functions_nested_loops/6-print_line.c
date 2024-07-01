56fec0d69d7367388c42d46685223192406f8683
Clement880 <clementlethata@gmail.com>
2024-07-01 09:15:58 -0700

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ *
+ * @n: print integer
+ *
+ */
+void print_line(int n);
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
