c7936f23599eaed95d3a2f23944ca978d5715221
Clement880 <clementlethata@gmail.com>
2024-07-04 07:10:50 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - this program will reset the value of n to 98
+ *
+ * @n: input
+ *
+ * Return: n on success
+ */
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
