36b66d855365243763e2a264a8f74f16ce5f5ca5
Clement880 <clementlethata@gmail.com>
2024-07-07 01:23:49 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry start point
  *
@@ -9,17 +8,19 @@
  */
 int main(void)
 {
-	int d;
-	
-	for (d = '0'; d <= '9'; d++)
+	char d = '0';
+
+	while (d <= '9')
 	{
 		putchar(d);
-		if (d != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar('\n');
+	if (d != '9')
+	{
+		putchar(',');
+		putchar(' ');
 	}
+	d++;
+	}
+	putchar('\n');
+
 	return (0);
 }
