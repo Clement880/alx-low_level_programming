96eb20f67437178b89a7478472a7c11efba94f47
Clement880 <clementlethata@gmail.com>
2024-07-07 06:03:01 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int tens1, units1, tens2, units2;
+
+	for (tens1 = 0; tens1 <= 9; tens1++)
+	{
+	for (units1 = 0; units1 <= 9; units1++)
+		{
+		for (tens2 = tens1; tens2 <= 9; tens2++)
+			{
+			int start_units2 = (tens1 == tens2) ? units1 + 1 : 0;
+
+					for (units2 = start_units2; units2 <= 9; units2++)
+				{
+					putchar(tens1 + '0');
+					putchar(units1 + '0');
+					putchar(' ');
+
+					putchar(tens2 + '0');
+					putchar(units2 + '0');
+
+					if (!(tens1 == 9 && units1 == 9 && tens2 == 9 && units2 == 9))
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	
+	putchar('\n');
+
+	return 0;
+}
