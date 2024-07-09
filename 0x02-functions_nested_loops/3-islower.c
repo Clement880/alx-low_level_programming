47b51382590d348b12ead0cc843e8cdf537c3f6c
Clement880 <clementlethata@gmail.com>
2024-07-09 07:26:03 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,15 @@
 
 /**
  * _islower _Checks for lowercase character
- * @C: The character to be checked
- * Return: 1 for lowercase character or 0for anything else
+ * @c: parameter to be checked
+ * Return: 1 for lowercase or 0 anything else
  */
 int _islower(int c)
 {
-	if (c >= 97 && c == 122)
+	if (c >= 'a' && c == '2')
 	{
-	
-	return (c);
+		return (1);
 	}
-	return (0);
+	else
+		return (0);
 }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @c: the character to be checked
+ * Return: (1) for alphabet character (0) for anything else
+ *
+ */
+int _isalpha(int c)
+{
+	if  ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
