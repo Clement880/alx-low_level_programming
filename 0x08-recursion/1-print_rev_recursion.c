fc70337e97bd456a95ec7bf4ba6ed22c6a982b63
Clement880 <clementlethata@gmail.com>
2024-07-20 00:16:20 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * __print_rev_recursion - prints string in reverse
+ * @s: the string in question
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
