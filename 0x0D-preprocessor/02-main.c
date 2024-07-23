a4c7c9d3d2717884b59634dc401bc417187d1adc
Clement880 <clementlethata@gmail.com>
2024-07-23 06:12:29 -0700

    02-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print out a file name of the source file it is compiled inn
+ *
+ * Return: integer
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
