1503b44eceb6bfad048d410e4289f44697e9adb2
Clement880 <clementlethata@gmail.com>
2024-07-23 06:32:35 -0700

    4-sum.c

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H_
+#define SUM_H_
+/**
+ * File: 4-sum
+ *
+ * @x: first input int
+ * @y: second input int
+ * description: sums up the sum of x and y
+ */
+#define SUM(x, y) (x + y)
+#endif
