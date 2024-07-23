64e6ffee47c6ec98b7bad59292dde6a93e9eb97a
Clement880 <clementlethata@gmail.com>
2024-07-23 06:26:15 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef LIKEMACRO_H_
+#define LIKEMACRO_H_
+
+/**
+ * file - 3-function_like_macro.h
+ *
+ * @x: value input (int)
+ *
+ * description: header file computes abs value
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
