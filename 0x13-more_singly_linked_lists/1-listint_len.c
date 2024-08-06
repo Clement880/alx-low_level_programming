b6e73a5e138c997d8028e5b86b09e906039b5cb2
Clement880 <clementlethata@gmail.com>
2024-08-06 00:03:49 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_t - lists number of elements in a linked listint_t list
+ *
+ * @h: head pointer
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	{
+	while (h != NULL)
+		count++;
+	}
+	return (count);
+}
