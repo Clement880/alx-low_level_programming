228b03aeb4affb9b661d52b4a043776270133a9b
Clement880 <clementlethata@gmail.com>
2024-08-09 11:17:14 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_bin - prints out recursive binary number
+ * representation
+ */
+void print_bin(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_bin(n >> 1);
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
+}
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
-	int bit_f = 0;
-	int i;
-
-	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		if ((n >> i) & 1)
-		{
-			_putchar('1');
-			bit_f = 1;
-		}
-			else if (bit_f)
-			{
-				 putchar('0');
-			}
+		print_bin(n);
 	}
-	if (!bit_f)
-	putchar('0');
 }
